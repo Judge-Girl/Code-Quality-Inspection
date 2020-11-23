0d0b4d43214153bd8c6853e4c2ad79b347657111
edisonhello <edisonhello@hotmail.com>
2020-11-24 02:48:34 +0800

    Fix to pass test

--- src/main/python/codingStyleAnalyzer/coding_style_analyzer.py
+++ src/main/python/codingStyleAnalyzer/coding_style_analyzer.py
@@ -9,10 +9,13 @@ except ImportError:
 
 
 def get_dump_result(path):
-    subprocess.run(['cppcheck', path, '--dump'])
+    subprocess.run(['cppcheck', path, '--dump'], stdout=subprocess.PIPE)
+
     dump_file_path = os.path.join(f'{path}.dump')
     dump_content = ''.join(open(dump_file_path, "r").readlines())
+    
     os.remove(dump_file_path)
+
     return dump_content
 
 
