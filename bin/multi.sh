8303a8ef92585d186b4e8a2640c7ab070ba4ef66
Giver <ryan01234keroro56789@yahoo.com.tw>
2020-12-28 13:50:52 +0800

    remove redundant files, move testing related scripts to bin

--- bin/multi.sh
+++ bin/multi.sh
@@ -1,4 +1,3 @@
-tot=$((0))
 for file in "$@"
 do
     bash regex.sh "$file"
--- bin/regex.sh
+++ bin/regex.sh
@@ -4,5 +4,5 @@ expand_cpp(){
 }
 code=$(($(expand_cpp < "$1" | grep -o "\([^0-9A-Za-z_]\(\(if\)\|\(case\)\|\(for\)\|\(while\)\|\(catch\)\)[^0-9A-Za-z_]\)\|[&][&]\|[|][|]\|[?]" | wc -l )))
 literal=$(($(expand_cpp < "$1" | egrep \"\(\\\.\|[^\"]\)*\" -o | grep -o "\([^0-9A-Za-z_]\(\(if\)\|\(case\)\|\(for\)\|\(while\)\|\(catch\)\)[^0-9A-Za-z_]\)\|[&][&]\|[|][|]\|[?]" | wc -l )))
-res=$((code-literal))
-echo "$1 $res"
+result=$((code-literal))
+echo "$1 $result"
