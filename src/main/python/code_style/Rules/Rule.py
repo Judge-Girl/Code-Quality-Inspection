6785fdd79fdcf43b8265b258c8fa4a72e1eb2fe4
edisonhello <edisonhello@hotmail.com>
2021-01-05 02:50:50 +0800

    Use abc

--- src/main/python/code_style/Rules/Rule.py
+++ src/main/python/code_style/Rules/Rule.py
@@ -1,15 +1,18 @@
 
+from abc import ABC, abstractmethod
 from typing import List
 
 from Rules.RuleResult import RuleResult
 
 
-class Rule:
+class Rule(ABC):
     def __init__(self, rule_name: str):
         self.rule_name = rule_name
 
+    @abstractmethod
     def apply_rule(self, xml, config) -> RuleResult:
-        raise Exception('Abstract method \'apply_rule\' not implemented')
+        raise NotImplemented()
 
+    @abstractmethod
     def child_result_collector(self, child_results: List[RuleResult], config) -> RuleResult:
-        raise Exception('Abstract method \'child_collector\' not implemented')
+        raise NotImplemented()
